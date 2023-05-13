1286b0b8f85f8283c6867701181f562fe9269d66
Jiahui Yu <97644710+KarenYu729@users.noreply.github.com>
2023-05-12 20:37:38 -0500

    Update settings.py

--- Django_intro_site/Django_intro_site/settings.py
+++ Django_intro_site/Django_intro_site/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": 'web_intro',
         "USER": 'root',
-        "PASSWORD": '348432',
+        "PASSWORD": '*****',
         "HOST": '127.0.0.1',
         "PORT": 3306,
     }
